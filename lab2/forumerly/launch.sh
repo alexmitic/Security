803e09266fc8de2538571d36eeef4f5c805fad5b
mohammad <maomer@kth.se>
2020-04-23 13:27:22 +0200

    Timing attack mitigation and some files

--- lab2/forumerly/launch.sh
+++ lab2/forumerly/launch.sh
@@ -1,4 +1,4 @@
 # DO NOT touch
 
 # run your container
-docker start lbs-services || docker run -it -p 6379:6379 -p 8087:8087 -p 27017-27019:27017-27019 --name lbs-services jacarte/lbs-services:latest
+sudo docker start lbs-services || sudo docker run -it -p 6379:6379 -p 8087:8087 -p 27017-27019:27017-27019 --name lbs-services jacarte/lbs-services:latest
